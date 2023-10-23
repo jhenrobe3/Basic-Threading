78b706f54cc6910a55f192fdbdc97d844b36ad15
jhenr <jhenr@JHENROBE>
2023-10-23 19:28:15 +0800

    Add project files.

---
+++ FrmBasicThread1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmBasicThread1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("- Thread Start -");
+
+            Thread ThreadA = new Thread(MyThreadClass.Thread1);
+            Thread ThreadB = new Thread(MyThreadClass.Thread1);
+
+            ThreadA.Name = "Thread A Process";
+            ThreadB.Name = "Thread A Process";
+
+            ThreadA.Start();
+            ThreadB.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+
+
+            Console.WriteLine("- End Thread -");
+            label1.Text = "- End Thread -";
+        }
+    }
+}
---
+++ FrmBasicThread1/MyThreadClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FrmBasicThread1
+{
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Thread Thread1 = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + Thread1.Name + " = " + i);
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
